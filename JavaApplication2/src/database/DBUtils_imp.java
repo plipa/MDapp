91306f8741ccc301a870c6a909c8e8255788ab00
Piotr Lipiak <piotr.lipiak@rec-global.com>
2014-06-13 00:13:34 +0200

    updates

--- JavaApplication2/src/database/DBUtils_imp.java
+++ JavaApplication2/src/database/DBUtils_imp.java
@@ -9,6 +9,7 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
@@ -19,7 +20,7 @@ Connection conn=null;
     
     
     public DBUtils_imp() throws SQLException {
-        String url = "jdbc:postgresql://192.168.1.21/project?user=test&password=test";
+        String url = "jdbc:postgresql://www.kubaplas.pl/project?user=doctor1&password=doctor";
         conn = DriverManager.getConnection(url);
     }
 
@@ -33,24 +34,26 @@ Connection conn=null;
 
     @Override
     public byte[] getNounce(int doctors_id) throws SQLException {
-        ResultSet s = executeQuery("select get_doctor_nonce()");
+        ResultSet s = executeQuery("select get_doctor_nonce("+Integer.toString(doctors_id)+")");
         byte[] nounce = s.getBytes(0);
         return nounce;
     }
 
     @Override
-    public Vector<Vector<String>> browseMedicine(String name, String type) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Vector<Vector<String>> browseMedicine(String name, String type) throws SQLException {
+        ResultSet s = executeQuery("select browse_medicines("+name+","+type+")");
+        return convertResultSetToVector(s);
     }
 
     @Override
-    public Vector<Vector<String>> browseDoctors(String name, String address, int license_number) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Vector<Vector<String>> browseDoctors(String name, String address, int license_number) throws SQLException {
+        ResultSet s = executeQuery("select browse_doctors("+name+","+address+","+String.valueOf(license_number)+")"); 
+        return convertResultSetToVector(s);
     }
 
     @Override
-    public void createPrescription(int doctor_id, int patient_id, int drug_id, int dosage, int unit, int quantity, byte[] signature) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void createPrescription(int doctor_id, int patient_id, int drug_id, int dosage, int unit, int quantity, byte[] signature) throws SQLException {
+        ResultSet s = executeQuery("select create_prescription("+doctor_id+","+drug_id+","+dosage+","+unit+","+quantity+","+signature+")"); 
     }
 
     @Override
@@ -59,8 +62,19 @@ Connection conn=null;
     }
 
     @Override
-    public Vector<Vector<String>> convertResultSetToVector(ResultSet set) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Vector<Vector<String>> convertResultSetToVector(ResultSet set) throws SQLException {
+        ResultSetMetaData rsmd = set.getMetaData();
+
+        int columnsNumber = rsmd.getColumnCount();
+        Vector ret = new Vector();
+        while(set.next()){
+            Vector<String> a = new Vector<String>();
+            for (int i = 0; i < columnsNumber; i++) {
+                a.add(String.valueOf(set.getObject(i)));
+            }
+            ret.add(a);
+        }
+        return ret;
     }
     
 }
