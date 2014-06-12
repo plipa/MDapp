1f710c79f2f6001cb1ed50f639a20c911bc48e8b
Piotr Lipiak <piotr.lipiak@rec-global.com>
2014-06-12 23:30:18 +0200

    added exceptions interface - error fix

--- JavaApplication2/src/database/DButils.java
+++ JavaApplication2/src/database/DButils.java
@@ -11,10 +11,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
 
-/**
- *
- * @author wro00571
- */
 public interface DButils {
     
     ResultSet executeQuery(String query) throws SQLException;
